aaab003d11ce8ed9d10bc3eb7c17de2c7779d06f
Muhammed Shittu <devshittu@gmail.com>
2023-04-30 23:30:50 +0100

    ch1 - Created the parse method.

--- ch1/recipe_extract_data_for_structured_strings_step1.py
+++ ch1/recipe_extract_data_for_structured_strings_step1.py
@@ -18,4 +18,16 @@ class PriceLog(object):
     def __repr__(self):
         return '<PriceLog ({}, {}, {})>'.format(self.timestamp, self.product_id, self.price)
         
-    
+    @classmethod
+    def parse(cls, text_log):
+        '''
+        Parse from a text log with the format
+        [<Timestamp>] - SALE - PRODUCT: <product id> - PRICE: $<price>
+        to a PriceLog object
+        '''
+        divide_it = text_log.split(' - ')
+        tmp_string, _, product_string, price_string = divide_it
+        timestamp = delorean.parse(tmp_string.strip('[]'))
+        product_id = int(product_string.split(':')[-1])
+        price = Decimal(price_string.split('$')[-1])
+        return cls(timestamp=timestamp, product_id=product_id, price=price)
