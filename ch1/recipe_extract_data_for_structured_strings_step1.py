ca0182ed5ccd1774b3df91db80fc3280b546cb7b
Muhammed Shittu <devshittu@gmail.com>
2023-04-30 23:30:30 +0100

    ch1 - Convert extraction script into a class.

--- ch1/recipe_extract_data_for_structured_strings_step1.py
+++ ch1/recipe_extract_data_for_structured_strings_step1.py
@@ -1,11 +1,21 @@
 import delorean
 from decimal import Decimal
 
-log = "[2018-05-05T11:07:12.267897] - SALE - PRODUCT: 1345 - PRICE: $09.99"
-divide_it = log.split(" - ")
-timestamp_string, _, product_string, price_string = divide_it
-product_id = int(product_string.split(":")[-1])
-price = Decimal(price_string.split("$")[-1])
-print(
-    f"product_id: {product_id}, timestamp_string: {timestamp_string},  price: {price}"
-)
+# log = "[2018-05-05T11:07:12.267897] - SALE - PRODUCT: 1345 - PRICE: $09.99"
+# divide_it = log.split(" - ")
+# timestamp_string, _, product_string, price_string = divide_it
+# product_id = int(product_string.split(":")[-1])
+# price = Decimal(price_string.split("$")[-1])
+# print(
+#     f"product_id: {product_id}, timestamp_string: {timestamp_string},  price: {price}"
+# )
+class PriceLog(object):
+    def __init__(self,timestamp, product_id, price):
+        self.timestamp = timestamp
+        self.product_id = product_id
+        self.price = price
+        
+    def __repr__(self):
+        return '<PriceLog ({}, {}, {})>'.format(self.timestamp, self.product_id, self.price)
+        
+    
